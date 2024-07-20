78d8fd26271edc469ba19763189f33e2a179a24a
kate <kate09030506@gmail.com>
2024-07-20 15:51:34 -0400

    link about

--- pages/urls.py
+++ pages/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
-from .views import HomeView 
+from .views import HomeView, AboutPageView
 
 urlpatterns = [
     path('', HomeView.as_view(), name='home'),
+    path('about/', AboutPageView.as_view(), name='about'),
 ]
-    
