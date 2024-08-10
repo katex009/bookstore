d18189685b09a983b15aef54315ca1d820a9d2d9
kate <kate09030506@gmail.com>
2024-08-10 17:34:08 -0400

    detail view

--- books/models.py
+++ books/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.urls import reverse
+from django.urls import reverse
 
 # Create your models here.
 
@@ -12,3 +14,5 @@ class Book(models.Model):
 
     def __str__(self):
         return self.title
+    def get_absolute_url(self):
+        return reverse('book_detail', args=[str(self.id)])
--- books/views.py
+++ books/views.py
@@ -1,9 +1,14 @@
 from django.shortcuts import render
 from .models import Book
 from django.views.generic import ListView
+from django.views.generic import DetailView
 
 # Create your views here.
 
 class BookListView(ListView):
     model = Book
     template_name = 'books/book_list.html'
+
+class BookDetailView(DetailView):
+    model = Book
+    template_name = 'books/book_detail.html'
