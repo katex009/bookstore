0e4e1ce11569932fdfacb0fcdefa8885b9bc8bf6
kate <kate09030506@gmail.com>
2024-09-07 16:39:27 -0400

    user permmissions

--- books/views.py
+++ books/views.py
@@ -2,12 +2,14 @@ from django.shortcuts import render
 from .models import Book
 from django.views.generic import ListView
 from django.views.generic import DetailView
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 # Create your views here.
 
-class BookListView(ListView):
+class BookListView(LoginRequiredMixin, ListView):
     model = Book
     template_name = 'books/book_list.html'
+    login_url = '/accounts/login' 
 
 class BookDetailView(DetailView):
     model = Book
