dfada63214dece49f4317e942da2a8b158962e0c
kate <kate09030506@gmail.com>
2024-09-14 17:52:05 -0400

    search added complete

--- books/views.py
+++ books/views.py
@@ -3,6 +3,7 @@ from .models import Book
 from django.views.generic import ListView
 from django.views.generic import DetailView
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.db.models import Q
 
 # Create your views here.
 
@@ -18,3 +19,9 @@ class BookDetailView(LoginRequiredMixin, DetailView):
 class SearchResultsView(ListView):
     model = Book
     template_name = 'books/search.html'
+
+    def get_queryset(self):
+        query = self.request.GET.get('q')
+        return Book.objects.filter(
+        Q(title__icontains=query) | Q(author__icontains=query)
+        )
